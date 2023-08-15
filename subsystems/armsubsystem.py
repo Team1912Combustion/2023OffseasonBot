1ad273176387e8f813d66e5af4dfb8d309800b47
Clark Rowley <clarkdavidrowley@gmail.com>
2023-08-14 19:14:30 -0500

    iAdd arm subsystem again

--- subsystems/armsubsystem.py
+++ subsystems/armsubsystem.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 # Copyright (c) FIRST and other WPILib contributors.
 # Open Source Software; you can modify and/or share it under the terms of
 # the WPILib BSD license file in the root directory of this project.
@@ -7,10 +8,16 @@ import commands2
 import wpimath.controller
 import wpimath.trajectory
 
+=======
+
+import wpilib
+import commands2
+>>>>>>> ae6b02a (iAdd arm subsystem again)
 import rev
 
 import constants
 
+<<<<<<< HEAD
 class ArmSubsystemPID(commands2.ProfiledPIDSubsystem):
   """A robot arm subsystem that moves with a motion profile."""
 
@@ -62,3 +69,54 @@ class ArmSubsystemPID(commands2.ProfiledPIDSubsystem):
     def _getMeasurement(self) -> float:
       return self.encoder.getDistance() + constants.ArmConstants.kArmOffsetRads
 
+=======
+class ArmSubsystem(commands2.SubsystemBase):
+    def __init__(self) -> None:
+        """Creates a new DriveSubsystem"""
+        super().__init__()
+
+        # The motors on the left side of the drive.
+        self.left = rev.CANSparkMax(constants.ArmConstants.kLeftMotorPort)
+        self.right = rev.CANSparkMax(constants.ArmConstants.kRightMotorPort)
+
+        # The left-side drive encoder
+        self.leftEncoder = self.left.getEncoder()
+        self.rightEncoder = self.right.getEncoder()
+
+        # We need to invert one side of the drivetrain so that positive voltages
+        # result in both sides moving forward. Depending on how your robot's
+        # gearbox is constructed, you might have to invert the left side instead.
+        self.right.setInverted(True)
+
+        # Create an object for our odometry, which will utilize sensor data to
+        # keep a record of our position on the field.
+        self.odometry = DifferentialDriveOdometry(
+            self.navx.getRotation2d(),
+            self.leftEncoder.getDistance(),
+            self.rightEncoder.getDistance(),
+        )
+        
+        self.leftEncoder.setPosition(0.)
+        self.rightEncoder.setPosition(0.)
+
+        self.target = 0.
+        self.kP = .002
+        self.kO = .002
+
+    def getTarget(self):
+        return self.target
+
+    def setTarget(self, target):
+        self.target = target
+
+    def error(self):
+        return 0.5 * (self.leftEncoder.getPosition() + self.rightEncoder.getPosition()) - self.getTarget()
+
+    def offset(self):
+        return self.leftEncoder.getPosition() - self.rightEncoder.getPosition()
+
+    def periodic(self):
+        error = self.meanposition() - self.target
+        leftMotorPower = self.kP * error + self.kO * self.offset
+        rightMotorPower = self.kP * error - self.kO * self.offset
+>>>>>>> ae6b02a (iAdd arm subsystem again)
